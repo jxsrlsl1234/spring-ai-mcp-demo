53adcdefc9d67670abc53e43aa2f3666bb906ba1
jxsrlsl1234 <jxsrlsl1234@jxsrlsl1234deMacBook-Pro.local>
2025-05-20 01:11:09 +0800

    修改LLM及spring-ai 版本

--- spring-boot-mcp-client-demo/src/main/java/com/kuaishou/ai/demo/client/PluginConfiguration.java
+++ spring-boot-mcp-client-demo/src/main/java/com/kuaishou/ai/demo/client/PluginConfiguration.java
@@ -3,8 +3,6 @@ package com.kuaishou.ai.demo.client;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.moonshot.MoonshotChatModel;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class PluginConfiguration {
 
     @Autowired
-    private MoonshotChatModel moonshotChatModel;
-    @Autowired
     private OllamaChatModel ollamaChatModel;
-
-
-    /**
-     * 有记忆能力的chatMemory,会将前文上下文信息都存在内存里，具体查看InMemoryChatMemory
-     *
-     * @return
-     */
-    @Bean
-    public InMemoryChatMemory chatMemory() {
-        return new InMemoryChatMemory();
-    }
 
 
     /**
@@ -39,10 +24,10 @@ public class PluginConfiguration {
      */
     @Bean
     public ChatClient chatClient(ChatMemory chatMemory, ToolCallbackProvider provider) {
-        return ChatClient.builder(moonshotChatModel)
+        return ChatClient.builder(ollamaChatModel)
                 .defaultTools(provider)
                 .defaultSystem("假设你是孙悟空，以孙悟空的口吻回答问题")
-                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
     }
 }
